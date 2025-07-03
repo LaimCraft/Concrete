433b5e2cf51dae3ea45307fca51a462cfd58be69
isaevisa05 <isaevisao55@gmail.com>
2025-07-03 21:37:05 +0300

    Update

--- src/main/java/ru/laimcraft/concrete/packet/Packet.java
+++ src/main/java/ru/laimcraft/concrete/packet/Packet.java
@@ -1,4 +1,15 @@
 package ru.laimcraft.concrete.packet;
 
 public class Packet {
+
+    public int length;
+    public int id;
+
+    public Packet() {
+
+    }
+
+    public void read() {
+
+    }
 }
